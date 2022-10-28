89f8914db77155cd262255ef84ebd0d86adc4c6f
John Hester <john@imandra.ai>
2022-10-28 18:44:25 -0500

    feat: experiment with caqti queries

--- main.ml
+++ main.ml
@@ -1,3 +1,5 @@
+open Lwt.Infix
+
 type rating = float [@@deriving show]
 
 type result =
@@ -85,60 +87,36 @@ module Game = struct
     p2.rating <- b'
 end
 
-module Chess_caqti = struct
-  let connection_url = "postgresql://localhost:5432"
+module Q = struct
+  open Caqti_request.Infix
+  open Caqti_type.Std
 
-  (* This is the connection pool we will use for executing DB operations. *)
-  let pool =
-    match
-      Caqti_lwt.connect_pool ~max_size:10 (Uri.of_string connection_url)
-    with
-    | Ok pool -> pool
-    | Error err -> failwith (Caqti_error.show err)
+  let player =
+    let open Player in
+    let encode {name; rating} = Ok (name,rating) in
+    let decode (name, rating) = Ok {name; rating} in
+    let rep = Caqti_type.(tup2 string float) in
+    custom ~encode ~decode rep
 
-  type todo = {
-    id: int;
-    content: string;
-  }
+  let create_playerreg =
+    unit ->. unit @@
+    {eos|
+      CREATE TEMPORARY TABLE playerreg (
+        name text NOT NULL,
+        rating real NOT NULL,
+      )
+    |eos}
 
-  type error = Database_error of string
+  let reg_player =
+    tup2 string float ->. unit @@
+    "INSERT INTO playerreg (name, rating) VALUES (?, ?)"
 
-  (* Helper method to map Caqti errors to our own error type.
-     val or_error : ('a, [> Caqti_error.t ]) result Lwt.t -> ('a, error) result Lwt.t *)
-  let or_error m =
-    match%lwt m with
-    | Ok a -> Ok a |> Lwt.return
-    | Error e -> Error (Database_error (Caqti_error.show e)) |> Lwt.return
+  let update_rating_ =
+    tup2 string float ->. unit @@
+    "UPDATE playerreg SET rating = ? WHERE name = ?"
 
-  let migrate_query =
-    Caqti_request.exec Caqti_type.unit
-      {| CREATE TABLE todos (
-          id SERIAL NOT NULL PRIMARY KEY,
-          content VARCHAR
-       )
-    |}
-
-  let migrate () =
-    let migrate' (module C : Caqti_lwt.CONNECTION) = C.exec migrate_query () in
-    Caqti_lwt.Pool.use migrate' pool |> or_error
-
-  let rollback_query = Caqti_request.exec Caqti_type.unit "DROP TABLE todos"
-
-  let rollback () =
-    let rollback' (module C : Caqti_lwt.CONNECTION) =
-      C.exec rollback_query ()
-    in
-    Caqti_lwt.Pool.use rollback' pool |> or_error
-
-  (* Stub these out for now. *)
-  let get_all () = failwith "Not implemented"
-
-  let add _content = failwith "Not implemented"
-
-  let remove _id = failwith "Not implemented"
-
-  let clear () = failwith "Not implemented"
 end
+
 
 let () =
   let andrew = Player.mk "andrew" in
